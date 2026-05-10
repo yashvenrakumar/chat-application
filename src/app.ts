4f55ed5cb5562eed40a3d8d65cf6505e292ae301
ADMIN <admin@ADMINs-MacBook-Pro.local>
2026-05-10 11:16:02 +0530

    user

--- src/app.ts
+++ src/app.ts
@@ -5,23 +5,19 @@ import { notFoundMiddleware } from "./middlewares/notFound.middleware";
 import { errorMiddleware } from "./middlewares/error.middleware";
 import apiRouter from "./routes";
 
-
 const app = express();
-
 
 app.use(
   cors({
     origin: "*",
-  })
+  }),
 );
 
 app.use(helmet());
 app.use(express.json());
 
-
 app.use("/api/v1", apiRouter);
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
-
 
 export default app;
